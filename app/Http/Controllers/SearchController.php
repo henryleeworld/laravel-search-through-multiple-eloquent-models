191e9fd0e4342684ba967bf54f3ce5b7aee2ed2f
Henry Lee <henryleeworld@gmail.com>
2023-06-01 00:04:35 +0800

    feat:升級至 laravel 10

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,21 +11,21 @@ class SearchController extends Controller
 {
     public function show() 
     {
-        $movie = '尚氣與十環傳奇';
-        $postFirst = Post::create(['title' => '黑寡婦']);
+        $movie = '《星際異攻隊3》';
+        $postFirst = Post::create(['title' => '亞當術士']);
         $postSecond = Post::create(['title' => $movie]);
         foreach (range(1, 10) as $i) {
-            $postFirst->comments()->create(['body' => $i . '一家人攜手作戰']);
-            $postSecond->comments()->create(['body' => $i . '充滿濃濃的東方元素']);
+            $postFirst->comments()->create(['body' => $i . '一旦任務失敗，整個異攻隊也可能被終結']);
+            $postSecond->comments()->create(['body' => $i . '堅強地各自奔向自己最後的命運']);
         }
-        $results = Search::add(Post::with('comments')->withCount('comments'), 'title')->get('尚氣與十環傳奇');
+        $results = Search::add(Post::with('comments')->withCount('comments'), 'title')->search($movie);
         echo $movie . '相關註解共' . ' ' . $results->first()->comments_count . ' ' . '筆' . PHP_EOL;
         
         $videoFirst = Video::create(['title' => $movie, 'published_at' => now()->addDay()]);
         $videoSecond = Video::create(['title' => $movie]);
         $results = Search::add(Post::whereNotNull('published_at'), 'title')
             ->add(Video::whereNotNull('published_at'), 'title')
-            ->get($movie);
+            ->search($movie);
         echo $movie . '相關影片共' . ' ' . count($results) . ' ' . '筆' . PHP_EOL;
     }
 }
